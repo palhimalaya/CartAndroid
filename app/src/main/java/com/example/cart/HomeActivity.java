ce29cb6997942bb047f41794776bd2d301689e71
Himalaya Pal <himalayapal123@gmail.com>
2021-08-10 16:01:25 +0545

    added fragment home function

--- app/src/main/java/com/example/cart/HomeActivity.java
+++ app/src/main/java/com/example/cart/HomeActivity.java
@@ -1,14 +1,54 @@
 package com.example.cart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.view.MenuItem;
+import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+import com.example.cart.Fragments.HomeFragment;
+import com.example.cart.Helper.helper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+public class HomeActivity extends helper implements View.OnClickListener {
+    BottomNavigationView bottomNavBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initView();
+        initListener();
+        bottomNavBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment home = null;
+                if (item.getItemId()==R.id.action_home){
+                    home = new HomeFragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,home).commit();
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void initView() {
+        bottomNavBar = findViewById(R.id.bottomNavigationView);
+
+    }
+
+    @Override
+    public void initListener() {
+
+        }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/com/example/cart/MainActivity.java
+++ app/src/main/java/com/example/cart/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.cart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.cart.Fragments.HomeFragment;
 import com.example.cart.Helper.helper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class MainActivity extends helper implements View.OnClickListener {
     TextView decrement;
     TextView increment;
     TextView counter;
+    Button nextActivity;
     int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +35,14 @@ public class MainActivity extends helper implements View.OnClickListener {
          decrement = findViewById(R.id.decrement);
          increment = findViewById(R.id.increment);
          counter = findViewById(R.id.counterValue);
+         nextActivity =findViewById(R.id.goNextActivity);
          }
 
     @Override
     public void initListener() {
       decrement.setOnClickListener(this);
       increment.setOnClickListener(this);
+      nextActivity.setOnClickListener(this);
     }
 
     @Override
@@ -40,6 +52,12 @@ public class MainActivity extends helper implements View.OnClickListener {
         }
         if (v.getId()==R.id.decrement){
             doDecrement();
+        }
+        if (v.getId()==R.id.goNextActivity){
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+
+
+            startActivity(intent);
         }
 
     }
